00658fe5cd31684bca54275ddf0ddb80ae29dc13
gabrielmacedo102 <147204498+gabrielmacedo102@users.noreply.github.com>
2025-09-04 12:52:56 -0300

    Commit
    Página inicial

--- src/PaginaInicial/PaginaInicial.tsx
+++ src/PaginaInicial/PaginaInicial.tsx
@@ -1,24 +1,79 @@
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View } from 'react-native';
+import colors from '../colors';
 
 const PaginaInicial = () => {
-    return (
-        <View style={s.MainView}>
-
-            <Text>Página inicial</Text>
-
-
+  return (
+    <View style={s.MainView}>
+      <View style={s.HeaderView}>
+        <Text style={s.TextTitulo}>Bem vindo, Gabriel!</Text>
+        <View style={s.roleView}>
+            <Text style={s.TextRole}>Administrador</Text>
         </View>
-    )
-}
+        <View style={s.UserImage}>
+            <></>
+        </View>
+      </View>
+      <View style={s.BodyView}>
+        <Text>Body</Text>
+      </View>
+      <View style={s.FootterView}>
+        <Text>Footer</Text>
+      </View>
+    </View>
+  );
+};
 
-const s =  StyleSheet.create({
+const s = StyleSheet.create({
+  MainView: {
+    flex: 1,
+  },
+  HeaderView: {
+    height: 80,
+    width: '100%',
+    borderBottomWidth: 1,
+    borderBottomColor: colors.gray200,
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 10
+  },
+  BodyView: {
+    flex: 1,
+  },
+  FootterView: {
+    height: 60,
+    width: '100%',
+    borderTopWidth: 1,
+    borderTopColor: colors.gray200
+  },
+  UserImage: {
+    borderRadius: 50,
+    width: 50,
+    height: 50,
+    backgroundColor: colors.gray200,
+    borderWidth: 1,
+    borderColor: colors.gray300
+    
+    
+  },
+  TextTitulo: {
+    fontSize: 22,
+    fontWeight: "bold",
+    height: "100%",
+    width: "40%",
+    
+    textAlignVertical: "center"
+    
+  },
+  roleView:
+  {
+    backgroundColor: colors.erro,
+    padding: 5,
+    borderRadius: 6
+  },
+  TextRole: {
+    color: "white"
+  }
+});
 
-    MainView:
-    {
-        flex: 1
-    }
-})
-
-
-
-export {PaginaInicial};
+export { PaginaInicial };
